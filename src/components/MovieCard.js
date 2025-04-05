188b718fb063ec8ec381230de8c340b7a6960ca9
AreebAl <shaikhareebali99@gmail.com>
2025-04-05 22:48:58 +0530

    make audible 0

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,7 +8,7 @@ const MovieCard = ({ movie }) => {
       {showTrailer ? (
         <div className="relative w-full h-full">
           <iframe
-            src={`${movie.trailer}?autoplay=1`} 
+            src={`${movie.trailer}?autoplay=1&mute=0`} 
             className="absolute top-0 left-0 w-full h-full rounded"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
