968f0a008cc9666f7ace3dc7c760ca9dc84b70b3
AreebAl <shaikhareebali99@gmail.com>
2025-04-06 16:35:40 +0530

    static resour handling

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -10,11 +10,7 @@ const app = express();
 
 
 console.log("dirname",__dirname)
-app.use(express.static(path.join(__dirname, '../netflix-clone/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../netflix-clone/build', 'index.html'));
-});
 
 // MongoDB connection
 mongoose.connect(process.env.DB_URL, {
@@ -33,6 +29,12 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/movies', moviesRouter);
 app.use('/api/auth', authRouter);
+
+app.use(express.static(path.join(__dirname, '../netflix-clone/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../netflix-clone/build', 'index.html'));
+});
 
 // Start server
 const PORT = process.env.PORT || 5000;
